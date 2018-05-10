45a009bd233ea4d7e6ca3bc4128bad6671144404
agata <agata@agata.local>
2018-05-11 00:06:07 +0900

    use Vue.js for preference

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,30 @@
 $(() => {
-    chrome.storage.local.get(["copy-issue", "auto-resolution"], function(settings) {
-        $('#copy-issue').prop('checked', settings["copy-issue"] || false);
-        $('#auto-resolution').prop('checked', settings["auto-resolution"] || false);
-    });
-
-    $("#copy-issue").on("change", () => {
-        const flg = $('#copy-issue').prop('checked');
-        chrome.storage.local.set({"copy-issue": flg});
-    });
-    $("#auto-resolution").on("change", () => {
-        const flg = $('#auto-resolution').prop('checked');
-        chrome.storage.local.set({"auto-resolution": flg});
+    chrome.storage.local.get(["copy-issue", "auto-resolution"], (settings) => {
+        const items = [
+            {
+                id: "copy-issue",
+                text: "別のプロジェクトに課題を複製",
+                enabled: settings["copy-issue"]
+            },
+            {
+                id: "auto-resolution",
+                text: "状態の完了時、自動的に完了理由を対応済みにする",
+                enabled: settings["auto-resolution"]
+            }
+        ];
+    
+        const app = new Vue({
+            el: '#app',
+            methods: {
+                change: (item) => {
+                    const setting = {};
+                    setting[item.id] = item.enabled;
+                    chrome.storage.local.set(setting);
+                }
+            },
+            data: {
+              items: items
+            }
+        });
     });
 });
