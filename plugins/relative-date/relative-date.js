7dc7377d0c17af063a84c5121007d117d82aa628
agata <agata@agata.local>
2018-05-21 09:46:57 +0900

    add relative date

---
+++ plugins/relative-date/relative-date.js
@@ -0,0 +1,36 @@
+(() => {
+    const main = () => {
+        if (!location.pathname === "/dashboard" && !location.pathname.match(/[/]projects[/][A-Z0-9_]+/)) {
+          return;
+        }
+      
+        const convert = () => {
+            var list = $(".stream-update__meta:not(.converted)").addClass("converted");
+            list.each(function() {
+              var $elem = $(this);
+              var dateString = $elem.find("abbr").attr("data-tooltip");
+              var parts = dateString.match(/([0-9]+)[-]([0-9]+)[-]([0-9]+)[ ]([0-9]+)[:]([0-9]+)[:]([0-9]+)/)
+              if (parts == null || parts.length < 6) { return; }
+              var converted = parts[4] + ":" + parts[5];
+              $elem.text(converted);
+            });
+        }
+      
+		const observer = new MutationObserver((records, observer) => {
+			convert();
+		});
+		observer.observe($('#timeline, #timeline-section').get(0), {
+            childList: true,
+            subtree: true
+        });
+
+        convert();
+    }
+
+    PowerUps.isEnabled("relative-date", (enabled) => {
+		if (enabled) {
+            main();
+		}
+    });
+
+})();
