4f92686be7b21aad9cf74b2c69f6e018cc0caa18
agata <>
2018-05-30 10:19:09 +0900

    support url preview in comments and observe DOMs

--- plugins/url-preview/url-preview.js
+++ plugins/url-preview/url-preview.js
@@ -1,84 +1,180 @@
 (() => {
+
+    const getDomain = (url) => {
+        if (!url) {
+            return "";
+        }
+        const parts = /https?[:][/][/]([^/]+)[/].*/.exec(url);
+        if (!parts || parts.length < 2) {
+            return "";
+        }
+        return parts[1];
+    }
+
     const getMeta = (url, callback) => {
         $.ajax(url, {
-            timeout : 5000, // 1000 ms
+            timeout : 10000,
             datatype:'text'
         }).then(function(data){
-            var h = $("<div>").append($.parseHTML(data));
+            const h = $("<div>").append($.parseHTML(data));
+            const title = h.find("meta[property='og:title']").attr("content");
+            // if a page has not title, don't show preview
+            if (!title) {
+                return null;
+            }
             const meta = {
-                title: h.find("meta[property='og:title']").attr("content") || h.find("meta[name='title']").attr("content") || h.find("title").text() || "",
+                title: title,
                 description: h.find("meta[property='og:description']").attr("content") || h.find("meta[name='description']").attr("content") || "",
                 image: h.find("meta[property='og:image']").attr("content") 
                     || h.find("link[rel='apple-touch-icon']").attr("href")
-                    || h.find("link[rel='shortcut icon']").attr("href")
+                    || h.find("link[rel='shortcut icon']").attr("href"),
+                url: url,
+                domain: getDomain(h.find("meta[property='og:url']").attr("content")) || getDomain(url)
             }
-            //<link rel="shortcut icon" href="//ssl.gstatic.com/docs/spreadsheets/favicon_jfk2.png">
             callback(meta)
         },function(jqXHR, textStatus) {
         });
     }
 
+    const ESCAPE_CHARS = {
+        '<': '&lt;',
+        '>': '&gt;',
+        '&': '&amp;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '`': '&#x60;'
+    };
+
+    const h = (html) => {
+        if (!html) return;
+        return html.replace(/[<>&"'`]/g, (match) => {
+          return ESCAPE_CHARS[match];
+        });
+    }
+
     const buildPreview = (meta) => {
-        return `
-            <div style="
-                max-width: 300px;
-                border: 1px solid #e7e0df; 
-                height: 60px; display: flex;
+        const $preview = $(`
+            <a href="${h(meta.url)}" target="_blank" style="
+                text-decoration: none;
             ">
-                <div style="background-position: 50%; background-size: cover; background-image: url(${meta.image}); width: 60px; height: 60px" />
                 <div style="
-                    display: flex; 
-                    flex-direction: column; 
-                    padding: 6px 12px; 
-                    max-width: calc(100% - 60px); 
-                    max-height: none;
-                    overflow: hidden;
+                    max-width: 600px;
+                    border: 1px solid #e7e0df; 
+                    height: 70px; 
+                    display: flex;
+                    margin-top: 10px;
                 ">
                     <div style="
-                        color: #4f4c4b;
-                        display: block;
-                        line-height: 22px;
-                        height: auto;
-                        white-space: nowrap;
-                        margin-bottom: 3px;
-                        font-weight: 600;
-                    ">${meta.title}</div>
+                        border-bottom: 1px solid #e7e0df; 
+                        border-right: 1px solid #e7e0df; 
+                        background-position: 50%; 
+                        background-size: cover; 
+                        background-image: url(${h(meta.image)}); 
+                        width: 69px; 
+                        height: 69px;
+                    " />
                     <div style="
-                        color: #4f4c4b;
-                        display: -webkit-box;
-                        -webkit-line-clamp: 2;
-                        -webkit-box-orient: vertical;
-                        max-height: 33px;
+                        display: flex; 
+                        flex-direction: column; 
+                        padding: 6px 12px; 
+                        max-width: calc(100% - 70px); 
+                        max-height: none;
                         overflow: hidden;
-                        text-overflow: ellipsis;
-                        backface-visibility: hidden;
-                        word-wrap: break-word;
-                        overflow-wrap: break-word;
-                        transform: translateZ(0);
-                        position: relative;
-                        margin-right: 5px;
-                        line-height: 1.4;
-                        margin-bottom: 3px;
-                    ">${meta.description}</div>
+                    ">
+                        <div style="
+                            font-size: 1.3rem;
+                            color: #4f4c4b;
+                            display: block;
+                            line-height: 22px;
+                            height: auto;
+                            white-space: nowrap;
+                            margin-bottom: 3px;
+                            font-weight: 600;
+                        ">${h(meta.title)}</div>
+                        <div style="
+                            font-size: 1.2rem;
+                            color: #b3aba9;
+                            display: -webkit-box;
+                            -webkit-line-clamp: 2;
+                            -webkit-box-orient: vertical;
+                            max-height: 19px;
+                            overflow: hidden;
+                            text-overflow: ellipsis;
+                            backface-visibility: hidden;
+                            word-wrap: break-word;
+                            overflow-wrap: break-word;
+                            transform: translateZ(0);
+                            position: relative;
+                            margin-right: 5px;
+                            line-height: 1.4;
+                            margin-bottom: 3px;
+                        ">${h(meta.description)}</div>
+                        <div style="
+                            line-height: 14px;
+                            font-size: 1rem;
+                            font-weight: 600;
+                            text-overflow: ellipsis;
+                            overflow: hidden;
+                            white-space: nowrap;
+                            color: #b3aba9;
+                        ">
+                            ${h(meta.domain)}
+                        </div>
+                    </div>
                 </div>
-            </divv>
-        `
+            </a>
+        `);
+        return $preview;
     }
     
     const insertPreview = (meta, selector) => {
-        $(selector).after(buildPreview(meta));
+        $(selector).append(buildPreview(meta));
     }
 
-    const main = () => {
-        console.log("url-preview");
-        $("#issueDescription a").each((index, elem) => {
-            const url = $(elem).attr("href");
-            console.log(url);
-            const meta  = getMeta(url, (meta) => {
-                insertPreview(meta, "#issueDescription");
-                console.log(meta.title);
+    const setupIssueDescription = () => {
+        $("#issueDescription").after($(`<div class="url-preview" style="margin-bottom: 10px;"></div>`));
+        const convert = () => {
+            const $previewContainer = $(".url-preview").empty();
+            $("#issueDescription a").each((index, elem) => {
+                const url = $(elem).attr("href");
+                const meta  = getMeta(url, (meta) => {
+                    if (meta) {
+                        insertPreview(meta, $previewContainer);
+                    }
+                });
             });
+        }
+		const observer = new MutationObserver((records, observer) => {
+			convert();
+		});
+		observer.observe($('#issueDescription').get(0), {
+            childList: true
         });
+
+        convert();
+    }
+
+    const setupIssueComments = () => {
+        const convert = () => {
+            $(".js_comment-container .comment-content a").each((index, elem) => {
+                const url = $(elem).attr("href");
+                const meta  = getMeta(url, (meta) => {
+                    if (meta) {
+                        insertPreview(meta, $(elem).closest(".comment-item__inner"));
+                    }
+                });
+            });
+        }
+		const observer = new MutationObserver((records, observer) => {
+            convert();
+        });
+		observer.observe($('#comments .comment-list').get(0), {
+            childList: true
+        });
+    }
+    const main = () => {
+        setupIssueDescription();
+        setupIssueComments();
     }
 
     PowerUps.isEnabled("url-preview", (enabled) => {
