c6fecbaaacdc84bc6e9990097ec8a4c3adfee4d2
agata <>
2018-06-04 10:20:37 +0900

    show backlog-card with iframe

--- plugins/backlog-card/backlog-card.js
+++ plugins/backlog-card/backlog-card.js
@@ -1,12 +1,56 @@
 (() => {
+
+    const PATTERN = /[/]view[/]([A-Z_0-9]+[-][0-9]+)/;
+
+    const show = (issueKey, target) => {
+        console.log($(target).position());
+        console.log($(target).offset());
+        const offset = $(target).offset();
+        offset.top = offset.top + 20;
+        getContainer()
+        .css(offset)
+        .attr("src", `/view/embed/${issueKey}`).show();
+    }
+
+    const hide = () => {
+        getContainer().attr("src", "").hide();
+    }
+
+    const build = () => {
+        const $card = $(`<iframe id="backlog-card" style="
+            width: 500px;
+            height: 350px;
+            z-index: 10000000000;
+            position: absolute;
+            border-color: #c7c0be;
+            border: 1px solid transparent;
+            filter: drop-shadow(0 0 5px rgba(0,0,0,.5));
+            left: 0;
+            top: 0;
+        ">`).appendTo("body");
+        return $card;
+    }
+
+    const getContainer = () => {
+        const $card = $("#backlog-card");
+        if ($card.length == 0) {
+            return build();
+        } 
+        return $card;
+    }
+
     const main = () => {
-        console.log("backlog-card");
         $(document).on({
-            'mouseenter' : () => {
-                console.log("mouseenter");
+            'mouseenter' : (event) => {
+                const url = $(event.target).attr("href");
+                const parts = PATTERN.exec(url);
+                if (parts && parts[1]) {
+                    const issueKey = parts[1];
+                    show(issueKey, event.target);
+                }
             },
             'mouseleave' : () => {
-                console.log("mouseleave");
+                hide();
             }
          }, "a");
     }
