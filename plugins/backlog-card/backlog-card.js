0eb43fcd6208f496fef3d40aa128c73d3c8e6c72
agata <>
2018-06-07 10:16:39 +0900

    refactoring

--- plugins/backlog-card/backlog-card.js
+++ plugins/backlog-card/backlog-card.js
@@ -9,32 +9,37 @@
             .appendTo("body")
 
         PowerUps.injectScript(`
-            setInterval(() => {
+            (() => {
                 const PATTERN = /[/]view[/]([A-Z_0-9]+[-][0-9]+)/;
-                const SELECTOR = [
+                const TARGET_SELECTOR = [
                     "#issueDescription a:not(.backlog-card-checked)",
                     ".comment-content a:not(.backlog-card-checked)",
                     ".wiki-content a:not(.backlog-card-checked)"
                 ].join(",");
+                
+                const watch = () => {
+                    jQuery(TARGET_SELECTOR).each((index, elem) => {
+                        const $elem = jQuery(elem);
+                        const url = $elem.attr("href");
+                        const [, issueKey] = PATTERN.exec(url) || [];
+                        if (issueKey) {
+                            const html = \`<iframe src="/view/embed/\${issueKey}">\`
+                            $elem.tooltipster({
+                                content: html,
+                                contentAsHTML: true,
+                                interactive: true,
+                                speed: 0,
+                                position: "right",
+                                theme: "tooltipster-backlog"
+                            });
+                            $elem.addClass("backlog-card-anchor");
+                        }
+                        $elem.addClass("backlog-card-checked");
+                    });
+                }
 
-                jQuery(SELECTOR).each((index, elem) => {
-                    const $elem = jQuery(elem).addClass("backlog-card-checked");
-                    const url = $elem.attr("href");
-                    const parts = PATTERN.exec(url);
-                    if (parts && parts[1]) {
-                        const issueKey = parts[1];
-                        const html = '<iframe src="/view/embed/' + issueKey + '">'
-                        $elem.tooltipster({
-                            content: html,
-                            interactive: true,
-                            contentAsHTML: true,
-                            speed: 0,
-                            position: "right",
-                            theme: "tooltipster-backlog"
-                        }).addClass("backlog-card-anchor");
-                    }
-                });
-            }, 1000);
+                setInterval(watch, 1000);
+            })();
         `);
     }
 
