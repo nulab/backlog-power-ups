bca7c642bf2b9ce7db10f34caf578b08635d7836
saki <sakihet@gmail.com>
2024-09-06 19:14:28 +0900

    fix extending issue description textarea feature

--- plugins/extend-desc/extend-desc.js
+++ plugins/extend-desc/extend-desc.js
@@ -1,25 +1,30 @@
 (() => {
-    const extend = () => {
-        document.querySelector("#descriptionTextArea").style.height = "480px"
+    const selector = "#descriptionTextArea";
+    const heightExtended = "480px";
+    const limit = 10;
+
+    const extend = (elem) => {
+        elem.style.height = heightExtended;
     }
 
-    const addIssueView = () => {
-        extend();
-    }
-
-    const editIssueView = () => {
-        extend();
+    const checkElement = async (selector) => {
+        for (let i = 0; i < limit && document.querySelector(selector) === null; i++) {
+            await new Promise(resolve => setInterval(resolve, 100));
+        }
+        return document.querySelector(selector);
     }
 
     const main = () => {
-        setTimeout(() => {
-            if (location.pathname.startsWith("/add/")) {
-                addIssueView();
-            } else if (location.pathname.match(/[/]view[/][A-Z_0-9]+[-][0-9]+[/]edit/)) {
-                editIssueView();
+        checkElement(selector).then((elem) => {
+            if (elem) {
+                if (location.pathname.startsWith("/add/") || location.pathname.match(/[/]view[/][A-Z_0-9]+[-][0-9]+[/]edit/)) {
+                    extend(elem);
+                }
+            } else {
+                console.log('failed to extend textarea');
             }
-        }, 0);
-    }
+        });
+    };
 
     PowerUps.isEnabled("extend-desc", (enabled) => {
         if (enabled) {
