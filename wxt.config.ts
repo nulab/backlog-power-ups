8ff8a5146ab514dc9bb575ef3872befd21a304a6
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-08-08 17:39:29 +0900

    setup power-ups lib

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "wxt";
 export default defineConfig({
     manifest: {
         default_locale: "ja",
-        name: "__MSG_ext_name__",
-        description: "__MSG_ext_description__",
+        name: "__MSG_app_name__",
+        description: "__MSG_app_description__",
+        permissions: ['storage']
     },
     modules: ["@wxt-dev/i18n/module", "@wxt-dev/auto-icons"],
 });
