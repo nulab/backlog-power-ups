5771b191023ca25b863ea63f0d15b2230d0f58f5
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-08-10 02:21:42 +0900

    refactor: extend-desc plugin

--- entrypoints/extend-desc.content/index.ts
+++ entrypoints/extend-desc.content/index.ts
@@ -1,67 +1,19 @@
-// @ts-nocheck
-
 export default defineContentScript({
-	matches: [
-		"https://*.backlog.jp/add/*",
-		"https://*.backlogtool.com/add/*",
-		"https://*.backlog.com/add/*",
-		"https://*.backlog.jp/view/*/edit",
-		"https://*.backlogtool.com/view/*/edit",
-		"https://*.backlog.com/view/*/edit",
-		"https://*.backlog.jp/find/*",
-		"https://*.backlogtool.com/find/*",
-		"https://*.backlog.com/find/*",
-	],
+	matches: defineMatches(["/add/*", "/view/*", "/user/*", "/gantt/*"]),
+	allFrames: true,
 	async main() {
-		const { PowerUps } = await import("@/utils/power-ups");
-		const selector = "#descriptionTextArea";
-		const heightExtended = "480px";
-		const limit = 10;
+		if (await isPluginDisabled("extend-desc")) {
+			return;
+		}
 
-		const updateHeight = (elem) => {
-			elem.style.height = heightExtended;
-		};
+		const EXTENDED_HEIGHT = "480px";
 
-		const checkElement = async (selector) => {
-			for (
-				let i = 0;
-				i < limit && document.querySelector(selector) === null;
-				i++
-			) {
-				await new Promise((resolve) => setInterval(resolve, 100));
-			}
-			return document.querySelector(selector);
-		};
+		observeQuerySelector("#descriptionTextArea", (el) => {
+			el.style.height = EXTENDED_HEIGHT;
+		});
 
-		const extend = () => {
-			if (
-				location.pathname.startsWith("/add/") ||
-				location.pathname.match(/[/]view[/][A-Z_0-9]+[-][0-9]+[/]edit/)
-			) {
-				checkElement(selector).then((elem) => {
-					if (elem) {
-						updateHeight(elem);
-					} else {
-						console.log("failed to extend textarea");
-					}
-				});
-			}
-		};
-
-		const main = () => {
-			extend();
-			const observer = new MutationObserver(() => {
-				extend();
-			});
-			observer.observe(document.getElementsByTagName("title")[0], {
-				childList: true,
-			});
-		};
-
-		PowerUps.isEnabled("extend-desc", (enabled) => {
-			if (enabled) {
-				main();
-			}
+		observeQuerySelector(".comment-editor__textarea", (el) => {
+			el.style.height = EXTENDED_HEIGHT;
 		});
 	},
 });
