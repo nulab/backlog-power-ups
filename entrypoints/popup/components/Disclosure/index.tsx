5a3d0254f544e8c594b5e7462577b9f573ce87aa
Ryoya Tamura <ryoya.tamura@nulab.com>
2025-08-12 02:41:00 +0900

    improve popup ui

--- entrypoints/popup/components/Disclosure/index.tsx
+++ entrypoints/popup/components/Disclosure/index.tsx
@@ -7,22 +7,37 @@ import styles from "./index.module.css";
 
 type Props = {
 	label: string;
-	badge?: string | number;
+	enabledBadge?: string | number;
+	disabledBadge?: string | number;
 	children: React.ReactNode;
 };
 
-const Component: React.FC<Props> = ({ label, badge, children }) => {
+const Component: React.FC<Props> = ({
+	label,
+	enabledBadge,
+	disabledBadge,
+	children,
+}) => {
 	return (
 		<Disclosure>
 			<DisclosureButton className={styles.button}>
-				<span>{label}</span>
-				{badge != null && (
-					<span
-						className={`${styles.badge} ${badge === 0 ? styles.Disabled : ""}`}
-					>
-						{badge}
-					</span>
-				)}
+				<span className={styles.label}>{label}</span>
+				<span className={styles.badges}>
+					{enabledBadge != null && (
+						<span
+							className={`${styles.badge} ${
+								enabledBadge === 0 ? styles.Disabled : ""
+							}`}
+						>
+							{enabledBadge}
+						</span>
+					)}
+					{disabledBadge != null && (
+						<span className={`${styles.badge} ${styles.Disabled}`}>
+							{disabledBadge}
+						</span>
+					)}
+				</span>
 			</DisclosureButton>
 			<DisclosurePanel static className={styles.panel}>
 				{children}
